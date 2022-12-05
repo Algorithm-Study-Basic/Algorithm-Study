56d38243d880c427859ea652dbea6c94f8a56ed1
kimseoyoon0 <seoyoon047@gmail.com>
2022-12-05 22:50:09 +0900

    12월 첫째주 1일차

--- src/main/java/com/study/algorithm/seoyeon/week2/ATM.java
+++ src/main/java/com/study/algorithm/seoyeon/week2/ATM.java
@@ -3,7 +3,7 @@ package com.study.algorithm.seoyeon.week2;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class ATM {
+public class Atm {
     public static void main(String args[]) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
--- src/main/java/com/study/algorithm/seoyeon/week2/rope.java
+++ src/main/java/com/study/algorithm/seoyeon/week2/rope.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Scanner;
 
-public class rope {
+public class Rope {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt(), w = 0;
