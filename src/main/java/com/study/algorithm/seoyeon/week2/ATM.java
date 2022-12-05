e94ce74d731a7c3f7a10c65f1545b18dd3fb85d2
kimseoyoon0 <seoyoon047@gmail.com>
2022-12-05 22:48:02 +0900

    12월 첫째주 1일차

--- src/main/java/com/study/algorithm/seoyeon/week2/ATM.java
+++ src/main/java/com/study/algorithm/seoyeon/week2/ATM.java
@@ -1,18 +1,20 @@
 package com.study.algorithm.seoyeon.week2;
+
 import java.util.Arrays;
 import java.util.Scanner;
+
 public class ATM {
     public static void main(String args[]) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         int[] pi = new int[n];
-        for(int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             pi[i] = scanner.nextInt();
         }
         Arrays.sort(pi);
         int add = 0;
         int result = 0;
-        for(int j = 0; j < pi.length; j++) {
+        for (int j = 0; j < pi.length; j++) {
             add += pi[j];
             result += add;
         }
