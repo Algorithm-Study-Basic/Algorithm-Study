52a51f331d9b6b28f6d9656edb87a7d05f3729d4
kimseoyoon0 <seoyoon047@gmail.com>
2022-12-15 21:26:42 +0900

    12월 15일 문제 풀이

---
+++ src/main/java/com/study/algorithm/seoyeon/week6/Hide_and_Seek.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+
+public class Hide_and_Seek {
+    public static void main(String args[]) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+        int[] Min = new int[100001];
+        Arrays.fill(Min, -1);    //초기값을 다 -1로 설정
+        System.out.println(BFS(N, K, Min));
+
+    }
+
+    public static int BFS(int N, int K, int[] Min) {
+        int nextN = N;
+        int[] status = new int[3];
+        Queue<Integer> q = new LinkedList<Integer>();
+        q.add(nextN);
+        Min[nextN] = 0;
+
+        while (!q.isEmpty() && nextN != K) {
+
+            nextN = q.poll(); //다음에 이동할 좌표들
+            status[0] = nextN - 1;     //뒤로 한칸
+            status[1] = nextN + 1;    //앞으로 한칸
+            status[2] = nextN * 2;    //순간이동
+
+            for (int i = 0; i < 3; i++) {
+
+                if (status[i] >= 0 && status[i] <= 100000) {
+
+                    if (Min[status[i]] == -1) {
+                        //처음 간 곳이라면 큐에 넣고 상태를 전 위치값 +1 을 해준다.
+                        q.add(status[i]);
+                        Min[status[i]] = Min[nextN] + 1;
+
+                    }
+                }
+            }
+        }
+        return Min[K];
+    }
+}
